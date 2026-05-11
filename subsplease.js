fc350355f65fd8e6cc7898006079258449078706
Bobrecay <83726115+Bobrecay@users.noreply.github.com>
2026-05-10 21:19:19 -0400

    Add files via upload

--- subsplease.js
+++ subsplease.js
@@ -1,55 +1,41 @@
 export default new class SubsPlease {
   base = 'https://subsplease.org/api/'
 
-  async getRomaji(anilistId) {
-    const query = `query ($id: Int) { Media (id: $id, type: ANIME) { title { romaji } } }`
-    const res = await fetch('https://graphql.anilist.co', {
-      method: 'POST',
-      headers: { 'Content-Type': 'application/json', 'Accept': 'application/json' },
-      body: JSON.stringify({ query, variables: { id: Number(anilistId) } })
-    })
-    const json = await res.json()
-    return json?.data?.Media?.title?.romaji ?? null
-  }
+  async single({ anilistId, titles, episode, fetch: fetchFn }) {
+    let title = titles?.[0] ?? ''
 
-  async single({ anilistId, titles, episode }) {
-    if (!titles?.length) return []
-
-    let title = titles[0]
     if (anilistId) {
-      const romaji = await this.getRomaji(anilistId)
-      if (romaji) title = romaji
+      try {
+        const res = await fetchFn('https://graphql.anilist.co', {
+          method: 'POST',
+          headers: { 'Content-Type': 'application/json', 'Accept': 'application/json' },
+          body: JSON.stringify({
+            query: `query ($id: Int) { Media (id: $id, type: ANIME) { title { romaji } } }`,
+            variables: { id: Number(anilistId) }
+          })
+        })
+        const json = await res.json()
+        title = json?.data?.Media?.title?.romaji ?? title
+      } catch {}
     }
 
     const query = title + (episode ? ` ${episode}` : '')
-    const url = `${this.base}?f=search&tz=America/New_York&s=${encodeURIComponent(query)}`
-
-    const res = await fetch(url)
+    const res = await fetchFn(`${this.base}?f=search&tz=America/New_York&s=${encodeURIComponent(query)}`)
     const data = await res.json()
 
     if (!data || typeof data !== 'object') return []
 
-    return this.map(data)
-  }
-
-  batch = this.single
-  movie = this.single
-
-  map(data) {
     const results = []
     for (const key in data) {
       const item = data[key]
       if (!item.downloads) continue
-
-      for (const download of item.downloads) {
-        if (download.res !== '1080') continue
-
-        const hash = download.magnet?.match(/btih:([a-fA-F0-9]+)/)?.[1]
+      for (const dl of item.downloads) {
+        if (dl.res !== '1080') continue
+        const hash = dl.magnet?.match(/btih:([a-fA-F0-9]+)/i)?.[1]
         if (!hash) continue
-
         results.push({
           title: `${item.show} - ${item.episode} (1080p)`,
-          link: download.magnet,
+          link: dl.magnet,
           hash,
           seeders: 0,
           leechers: 0,
@@ -64,6 +50,9 @@ export default new class SubsPlease {
     }
     return results
   }
+
+  batch = this.single
+  movie = this.single
 
   async test() {
     try {
